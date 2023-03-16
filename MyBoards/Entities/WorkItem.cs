50487a9361bbabfc618725ac657799ac611ad559
mateusz <mateuszbronclik@gmail.com>
2023-03-16 12:48:22 +0100

    Added: Entities

---
+++ MyBoards/Entities/WorkItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace MyBoards.Entities
+{
+    public class WorkItem
+    {
+        public string State { get; set; }
+        public string Area { get; set; }
+        public string IterationPath { get; set; }
+        public int Priority { get; set; }
+        //Epic
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set;}
+        // Issue
+        public  decimal Efford { get; set; }
+        //Task
+        public string Activity { get; set; }
+        public decimal RemainingWork { get; set; }
+        public string Type { get; set; }
+
+    }
+}
