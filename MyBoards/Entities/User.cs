29c1f79d7b89ac46aa5ea73ab3cae0149ead3218
mateusz <mateuszbronclik@gmail.com>
2023-03-17 14:57:11 +0100

    Added: DbContext and config for MSSQL

--- MyBoards/Entities/User.cs
+++ MyBoards/Entities/User.cs
@@ -1,6 +1,13 @@
-﻿namespace MyBoards.Entities
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Diagnostics;
+
+namespace MyBoards.Entities
 {
     public class User
     {
+        public  string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Email { get; set; }   
     }
 }
