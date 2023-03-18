72043a22d3c4ab958b2269f1d66bdc798dcde994
mateusz <mateuszbronclik@gmail.com>
2023-03-18 16:31:10 +0100

    Added: Refactor MyBoardContext

--- MyBoards/Entities/MyBoardsContext.cs
+++ MyBoards/Entities/MyBoardsContext.cs
@@ -14,6 +14,18 @@ namespace MyBoards.Entities
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Adress> Addresses { get; set; }
 
-
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        { 
+            modelBuilder.Entity<WorkItem>(eb =>
+            {
+                eb.Property(wi => wi.State).IsRequired();
+                eb.Property(wi => wi.Area).HasColumnType("varchar(200)");
+                eb.Property(wi => wi.IterationPath).HasColumnName("Iteration_Path");
+                eb.Property(wi => wi.Efford).HasColumnType("decimal(5,2)");
+                eb.Property(wi => wi.EndDate).HasPrecision(3);
+                eb.Property(wi => wi.Activity).HasMaxLength(200);
+                eb.Property(wi => wi.RemainingWork).HasPrecision(14,2);
+            });
+        }
     }
 }
