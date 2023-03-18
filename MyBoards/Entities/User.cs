4878038fbe21954f888962e4fc24dfb700eba79d
mateusz <mateuszbronclik@gmail.com>
2023-03-18 16:01:46 +0100

    Added: PK for entities

--- MyBoards/Entities/User.cs
+++ MyBoards/Entities/User.cs
@@ -5,9 +5,9 @@ namespace MyBoards.Entities
 {
     public class User
     {
+        public Guid Id { get; set; }
         public  string FirstName { get; set; }
         public string LastName { get; set; }
-
         public string Email { get; set; }   
     }
 }
